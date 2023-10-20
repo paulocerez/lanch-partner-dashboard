cd713d63f9ad32a14020e348614e19af527cd030
Tobias Budig <tobias_b@outlook.de>
2023-10-20 15:55:15 +0200

    Update page.tsx

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,6 +3,7 @@ import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { auth } from '../firebase';
+import Image from 'next/image';
 
 export default function Signup() {
   const [signupEmail, setEmail] = useState('');
@@ -30,13 +31,16 @@ export default function Signup() {
         <body class="h-full">
         ```
       */}
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="flex min-h-full flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
+          <div className="flex justify-center">
+            <Image
+              src="/lanch_logo_with_text.png"
+              alt="LANCH Logo"
+              width="150"
+              height="150"
+            />
+          </div> 
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Regestrieren
           </h2>
@@ -46,7 +50,7 @@ export default function Signup() {
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Emailadresse
+                Email
               </label>
               <div className="mt-2">
                 <input
