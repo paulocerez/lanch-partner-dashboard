997abb44d6f1d9a579c56a30ac4cd86b08827de3
Tobias Budig <tobias_b@outlook.de>
2023-11-13 14:45:37 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,24 +4,24 @@ import type { NextRequest } from "next/server";
 export async function middleware(request: NextRequest, response: NextResponse) {
   const session = request.cookies.get("session");
 
-  //Return to /login if don't have a session
-  if (!session) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  // //Return to /login if don't have a session
+  // if (!session) {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
 
-  //Call the authentication endpoint
-  const responseAPI = await fetch(`${request.nextUrl.origin}/api/auth`, {
-    headers: {
-      Cookie: `session=${session?.value}`,
-    },
-  });
+  // //Call the authentication endpoint
+  // const responseAPI = await fetch(`${request.nextUrl.origin}/api/auth`, {
+  //   headers: {
+  //     Cookie: `session=${session?.value}`,
+  //   },
+  // });
 
-  //Return to /login if token is not authorized
-  if (responseAPI.status !== 200) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  // //Return to /login if token is not authorized
+  // if (responseAPI.status !== 200) {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
 
-  return NextResponse.next();
+  return NextResponse.redirect(new URL("/login", request.url));
 }
 
 //Add your protected routes
