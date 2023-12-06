23e0f1b001d0c584fe1a65c65b56bc67a86f47b3
Tobias Budig <tobias_b@outlook.de>
2023-12-06 14:10:53 +0100

    added Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,14 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ApolloWrapper } from "./ApolloWrapper";
 import { Analytics } from '@vercel/analytics/react';
+import GoogleAnalytics from './googleAnalytics';
+import Head from 'next/head';
+import Script from 'next/script';
 
 
 const inter = Inter({ subsets: ['latin'] })
+
+const GTM_ID = "G-TH36C8PKPK"
 
 export const metadata: Metadata = {
   title: 'LANCH Partner Dashboard',
@@ -20,6 +25,15 @@ export default function RootLayout({
   return (
 
     <html lang="en" className="h-full bg-white">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+    </Script>
       <body className="h-full p-8">
       <ApolloWrapper>
 
@@ -27,6 +41,7 @@ export default function RootLayout({
           <Analytics />
       </ApolloWrapper>
       </body>
+  
     </html>
   )
 }
