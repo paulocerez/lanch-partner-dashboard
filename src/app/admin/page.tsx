61b4207c8f8b0e7070d8899423ebf34d344f00c9
paulocerez <paulo.ramirez@code.berlin>
2024-02-15 11:07:21 +0100

    fix: admin route

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,42 +1,49 @@
 'use client'
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, useEffect, useLayoutEffect } from 'react';
+import { sessionStatus } from '../utils/session';
 import HeaderComponent from "../dashboard/_components/dashboard-helpers/header";
 import AdminUserList from "../dashboard/_components/admin/admin_user_list";
-
 
 // Define the structure for the response data
 interface AuthResponse {
   message: string;
 }
 
-export default function LoginPage() {
-  const [username, setUsername] = useState<string>('');
+export default function Admin() {
+  // const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  // executes before content is rendered
+  // if (password === 'partner-dashboard-2024') {
+  //   setIsAuthenticated(true);
+  // }
+
+
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   
     // POST request to the server
-    const response = await fetch('/api/admin-auth', {
+    const response = await fetch('/api/admin', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ password }),
     });
+    console.log(password)
+
   
     // Only attempt to parse the response body as JSON if the status is 200 OK
     if (response.ok) {
-      const data: AuthResponse = await response.json();
-  
-      // Show an alert window with the response message
-      alert(data.message);
-  
+      const data = await response.json();
+      console.log(data)
       setIsAuthenticated(true);
     } else {
       // For all other status codes, assume authentication failed
       alert('Authentication failed');
+      console.log("Error during fetch: ", response.statusText)
     }
   };
 
@@ -44,26 +51,32 @@ export default function LoginPage() {
     <div>
       <HeaderComponent/>
       {isAuthenticated ? (
-        // If authenticated, show the AdminUserList
         <AdminUserList/>
       ) : (
-        // If not authenticated, show the login form
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-          />
-          <button type="submit">Log In</button>
-        </form>
+        <div className='flex justify-center align-center items-center'>
+
+        <div className='bg-white flex justify-center rounded-xl w-72 shadow-xl'>
+          <form onSubmit={handleSubmit} className='flex flex-col space-y-4 m-8 text-center'>
+          <h1 className='decoration-solid font-bold'>Enter admin password</h1>
+            {/* <input
+            className='p-2 bg-slate-100 rounded-md' 
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)}
+              /> */}
+            <input
+            className='p-2 bg-slate-100 rounded-md text-md'
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+              />
+              <button type='submit' className='bg-blue-500 text-white rounded-md p-2'>Log in</button>
+          </form>
+        </div>
+        </div>
       )}
     </div>
   );
-}
+} 
---
+++ src/app/api/admin/route.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export async function POST (req: NextApiRequest, res: NextApiResponse){
+  const { password } = req.body;
+  // const storedUsername = process.env.ADMIN_USERNAME;
+  const storedPasswordHash = process.env.ADMIN_PASSWORD_HASH;
+  console.log(req.body)
+
+if (req.method !== 'POST') {  
+  // if (username === storedUsername) {
+    // Check if storedPasswordHash is defined
+    if (storedPasswordHash) {
+      // Compare the provided password with the stored hash
+      const isMatch = await bcrypt.compare(password, storedPasswordHash);
+      if (isMatch) {
+        // Passwords -> match
+        return res.status(200).json({ message: 'Authentication successful' });
+      } else {
+        // Passwords -> no match
+        return res.status(401).json({ message: 'Password is not correct' });
+      }
+    } else {
+      // storedPasswordHash is undefined
+      return res.status(500).json({ message: 'Server error' });
+    }
+  // } else {
+  //   // Usernames -> no match
+  //   return res.status(401).json({ message: 'Authentication failed' });
+  // }
+} else {
+  // Method -> not allowed
+  return res.status(405).json({ message: 'Method not allowed' });}
+}
