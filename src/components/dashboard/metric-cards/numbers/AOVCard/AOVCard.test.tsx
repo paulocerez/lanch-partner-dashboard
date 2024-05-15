c9715960825c62832fdda2bce77ebede1dcd7339
paulocerez <paulo.ramirez@web.de>
2024-05-15 19:08:48 +0200

    feat: add AOVCard test

--- src/components/dashboard/metric-cards/numbers/AOVCard/AOVCard.test.tsx
+++ src/components/dashboard/metric-cards/numbers/AOVCard/AOVCard.test.tsx
@@ -1,15 +1,70 @@
 import { describe, expect, it, vi } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { AOVCard } from "@/components/dashboard/metric-cards/numbers/AOVCard/AOVCard";
 import { CardProps } from "../../cardProperties";
-import { useTotalGMVData } from "../useTotalGMVData";
-import { calculateAverage } from "@/utils/metricCalculations";
+import { GET_TOTAL_GMV } from "@/utils/gqlQueries";
+import { MockedProvider } from "@apollo/client/testing";
 
-// Mocking useTotalGMVData hook and calculateAverage utility function
-const mockUseTotalGMVData = vi.fn().mockImplementation(useTotalGMVData);
-const mockCalculateAverage = vi.fn().mockImplementation(calculateAverage);
+// mocks array > each defines the mock response of a single operation
+const successMock = [
+  {
+    request: {
+      query: GET_TOTAL_GMV,
+      variables: {
+        _vendor_ids: ["DE_Berlin_0001", "DE_Hamburg_0001"],
+        _fromDate: new Date(2024, 1, 1).toISOString(),
+        _toDate: new Date(2024, 1, 8).toISOString(),
+        _order_source_names: ["Lieferando", "Uber Eats"],
+      },
+    },
+    result: {
+      data: {
+        api_partner_dashboard_api_pd_food_orders_aggregate: {
+          aggregate: {
+            count: "50",
+            sum: {
+              gmv: "1000",
+            },
+          },
+        },
+      },
+    },
+  },
+];
 
-describe("AOVCard", () => {
+const loadingMock = [
+  {
+    request: {
+      query: GET_TOTAL_GMV,
+      variables: {
+        _vendor_ids: ["DE_Berlin_0001", "DE_Hamburg_0001"],
+        _fromDate: new Date(2024, 1, 1).toISOString(),
+        _toDate: new Date(2024, 1, 8).toISOString(),
+        _order_source_names: ["Lieferando", "Uber Eats"],
+      },
+    },
+    result: {
+      loading: true,
+    },
+  },
+];
+
+const errorMock = [
+  {
+    request: {
+      query: GET_TOTAL_GMV,
+      variables: {
+        _vendor_ids: ["DE_Berlin_0001", "DE_Hamburg_0001"],
+        _fromDate: new Date(2024, 1, 1).toISOString(),
+        _toDate: new Date(2024, 1, 8).toISOString(),
+        _order_source_names: ["Lieferando", "Uber Eats"],
+      },
+    },
+    error: new Error("An error occurred"),
+  },
+];
+
+describe("AOVCard", async () => {
   const mockProps: CardProps = {
     vendorIds: ["DE_Berlin_0001", "DE_Hamburg_0001"],
     dateRange: {
@@ -19,19 +74,41 @@ describe("AOVCard", () => {
     orderPortal: ["Lieferando", "Uber Eats"],
   };
 
-  it("should display a loading state when data is being fetched", () => {
-    render(<AOVCard {...mockProps} />);
-    expect(screen.getByText("Durchschn. Warenkorb")).toBeDefined();
+  it("should display the CardComponent with the correct average value when data is successfully fetched", async () => {
+    render(
+      <MockedProvider mocks={successMock} addTypename={false}>
+        <AOVCard {...mockProps} />
+      </MockedProvider>
+    );
+
+    await waitFor(() => {
+      const titleElement = screen.getByText("Durchschn. Warenkorbwert");
+      expect(titleElement).toBeDefined();
+
+      const metricElement = screen.getByText("20.00€");
+      expect(metricElement).toBeDefined();
+    });
   });
 
-  it("should display an error message when there is an error fetching data", () => {
-    render(<AOVCard {...mockProps} />);
-    expect(screen.getByText("Error loading data")).toBeDefined();
+  it("should display a loading state when data is being fetched", async () => {
+    render(
+      <MockedProvider mocks={successMock} addTypename={false}>
+        <AOVCard {...mockProps} />
+      </MockedProvider>
+    );
+    expect(
+      screen.getByText("Durchschn. Warenkorb wird geladen...")
+    ).toBeDefined();
   });
 
-  it("should display the CardComponent with the correct average value when data is successfully fetched", () => {
-    render(<AOVCard {...mockProps} />);
-    expect(screen.getByText("Durchschn. Warenkorbwert")).toBeDefined();
-    expect(screen.getByText("€100")).toBeDefined();
+  it("should display an error message when there is an error fetching data", async () => {
+    render(
+      <MockedProvider mocks={errorMock} addTypename={false}>
+        <AOVCard {...mockProps} />
+      </MockedProvider>
+    );
+    await waitFor(() => {
+      expect(screen.getByText("Error loading data")).toBeDefined();
+    });
   });
 });
--- src/components/dashboard/metric-cards/numbers/useTotalGMVData.test.ts
+++ src/components/dashboard/metric-cards/numbers/useTotalGMVData.test.ts
@@ -1,8 +1,11 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeAll, afterAll } from "vitest";
+import { handlers } from "@/__tests__/mocks/graphqlHandlers";
+import { server } from "@/__tests__/mocks/server";
+import { MockedProvider, createMockClient } from "@apollo/client/testing";
 
-describe('getTotalGMV query', () => {
-	it('fetches the total GMV query correctly and returns the expected data object', async() => {
-		const response  = await 
-		expect("", ).toBe()
-	}
-})
+describe("useTotalGMVData hook", () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
+  it("fetches the total GMV query correctly and returns the expected data object", async () => {});
+});
